ca5f508bb8677a431ec6f72203878afca34e8ac3
Thomas O'Connor <thomas.o@outlook.com>
2024-03-03 20:26:46 +1100

    Complete Re-Factor, Move codebase to OOP style code.

---
+++ main.py
@@ -0,0 +1,96 @@
+#!/bin/python
+'''Speedtest / iPerf3 Automation CLI. Marries the Libraries we have created together into something we can control with command flags'''
+from datetime import datetime
+import getopt
+import sys
+from wrappers import Speedtest
+from wrappers import Iperf3Auto
+
+HELP = \
+    f"\nSpeedTest-Automated\n"\
+    f"2024 TOC\n"\
+    f"-h --help\n"\
+    f"-v --value\n"
+TELSTRA_MELBOURNE = ['Telstra - Melbourne',12491]
+TELSTRA_SYDNEY = ['Telstra - Sydney',12492]
+
+def print_results(df_total,output_file):
+    '''Prints output results of testing on screen, and saves them to a prefined CSV'''
+    df_total.to_csv(output_file)
+    print(df_total[['Server Name','Download Bandwidth (Mbps)','Upload Bandwidth (Mbps)']])
+    print(f'Results Found in {output_file}')
+
+
+def run_test(mode='speedtest',server=['Telstra - Melbourne',12491],num_of_runs=3):
+    '''Function to run the Actual Tests'''
+
+    if 's' in mode.lower():
+        df = Speedtest().run_test(server_name=server[0],server_id=server[1],num_of_runs=num_of_runs)
+    elif 'i' in mode.lower():
+        df = Iperf3Auto().run_test(server=server[1],num_of_runs=num_of_runs)
+
+    
+    return df
+
+
+def main():
+    '''Primary Function of Script, controls cli control logic, and interprets flags for our use :)
+    '''
+
+    # Import Command Arguments
+    args=sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(args,"hl:s:o:p:n:m:",["help","server-id=","outputfile=","location=","pass_threshold="])
+    except getopt.GetoptError:
+        print(f'INVALID SYNTAX:\n {HELP}')
+
+    #Call Default CLI Values
+    num_of_runs = 3
+    mode = 'speedtest'
+    server = ['Telstra - Melbourne',12491]
+    output_file = f'{mode.title()}-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+
+    #Switch Statement - Dirty Because of older python versions
+    # TODO When  Python 3.8 reaches EOL, update this to match function
+    if len(opts) > 0:
+        for opt, arg in opts:
+            #Help
+            if opt == '-h':
+                print(HELP)
+                sys.exit()
+            #iPerf or Speedtest mode
+            elif opt == '-m':
+                if 'i' in arg.lower():
+                    mode = 'iPerf'
+                else:
+                    mode = 'Speedtest'
+            #Number of Runs
+            elif opt == '-n':
+                num_of_runs = int(arg)
+            #Server IP or ID
+            elif opt == '-s':
+                server = [f'Custom {mode} Server',arg]
+            #Location option (SpeedTest Only)
+            elif opt == '-l':
+                print(arg)
+                #if arg == 'MEL' or 'mel' or 'Melbourne' or 'melbourne':
+                if 'mel' in arg.lower():
+                    server = ['Telstra - Melbourne',12491]
+                elif 'syd' in arg.lower():
+                    server = ['Telstra - Sydney',12492]
+            #Output File
+            elif opt =='-o':
+                output_file=f'{arg}.csv'
+            else:
+               print(f"ERROR: Unknown argument \n {HELP}")
+               sys.exit()
+            #Push Flags to function
+    
+    df = run_test(mode=mode,server=server,num_of_runs=num_of_runs)
+    print_results(df_total=df,output_file=output_file)
+            
+
+    # Initial test Code.
+
+if __name__ == "__main__":
+    main()
---
+++ wrappers/iperf3_auto.py
@@ -0,0 +1,75 @@
+'''simple module to batch run iPerf tests using the iperf3'''
+# Import the required modules
+import iperf3 # This Library is doing the leg work ere
+import pandas as pd
+from datetime import datetime
+
+class Iperf3Auto:
+    """ Main Class of File
+    the Goal of this class is to:
+     -  emulate how our speedtest class is making tests, and mimic that using iperf3
+     - return a pandas dataframe that we can use to graph data"""
+    def __init__(self) -> None:
+        pass
+    def iperf_test(self,server, duration, reverse):
+        ''' Pushes Through the iPerfTests using iPerf'''
+        # Create an iperf3 client object
+        client = iperf3.Client()
+        # Set the server address, the test duration, and the reverse mode
+        client.server_hostname = server
+        client.duration = duration
+        client.reverse = reverse
+        # Run the test and get the result object
+        result = client.run()
+        # Check if the test was successful
+        if result.error:
+            print(f'Error: {result.error}')
+        else:
+            # Create a pandas dataframe with the result attributes
+            df = pd.DataFrame([result.__dict__])
+            # Filter the dataframe for desired Columns only, there's a lot of data in here
+            df = df.filter(['timesecs','num_streams','sent_bps','received_bps'])
+            # Convert the bits_per_second to megabits_per_second and round to two decimals
+            df['datetime'] = datetime.fromtimestamp(int(df['timesecs']))
+            df['sent_mbps'] = round(df['sent_bps'] / 1e6, 2)
+            df['recieved_mbps'] = round(df['received_bps'] / 1e6, 2)
+            # Drop the bits_per_second columns
+            df = df.drop(['sent_bps', 'received_bps','timesecs'], axis=1)
+            # Return the filtered dataframe
+            return df
+    def run_test(self,server,num_of_runs=3):
+        '''Basic Control Logic to get Mimic that speedtest.net behaviour, requires server to run'''
+        dfs = []
+        for i in range(num_of_runs):
+            print(f'Running {server}: iPerf {i + 1}')
+
+            # Mimic a Speedtest.net Download Test, then append the Download type to it.
+            df_download = self.iperf_test(server,10,True)
+            
+            # Mimic a Speedtest.net Upload Test, then append Upload to the result
+            df_upload = self.iperf_test(server,10,False)
+
+            # Combine both DataFrames into a combined one with the relevant data we need. 
+            #Note we use 'recieved_mbps' to measure traffic that has actually gone across the link
+            df_combo = pd.DataFrame()
+            df_combo['Mode'] = 'iPerf3'
+            df_combo['Server Name'] = f'{server}'
+            df_combo['datetime'] = df_upload['datetime']
+            df_combo['Download Bandwidth (Mbps)'] = df_download['recieved_mbps']
+            df_combo['Upload Bandwidth (Mbps)'] = df_upload['recieved_mbps']
+            df_combo['Number of Streams (DL)'] = df_download['num_streams']
+            df_combo['Number of Streams (UL)'] = df_upload['num_streams']
+
+            
+            print(df_combo[['Server Name','Download Bandwidth (Mbps)','Upload Bandwidth (Mbps)']])
+
+            dfs.append(df_combo)
+        # Concat ALL Dataframes and return to 
+        return pd.concat(dfs,ignore_index=True)
+            
+
+
+
+
+
+            
